19cbb44262bfd4e7d663f7018e5f39b3b6a2cb94
Gideon <chima.sima@gmail.com>
2019-04-15 21:53:16 +0100

    [#165365182] Add API endpoint to update user

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/api/v1/auth/signup', userController.createUser);
 router.post('/api/v1/auth/signin', userController.LoginUser);
+router.put('/api/v1/users/:id', userController.updateUser);
 
 module.exports = router;
