d9ba3c69c66c93c83766850d9c999bc0c3504514
Gideon <chima.sima@gmail.com>
2019-04-15 03:04:48 +0100

    [finishes #165075086] Create API endepoint for user sign in

---
+++ Server/controllers/user.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-else-return */
+/* eslint-disable class-methods-use-this */
+const fs = require('fs');
+
+const data = fs.readFileSync('./model/User.json');
+const users = JSON.parse(data);
+
+class UsersController {
+
+  // LOGIN USER -- FUNCTION
+  LoginUser(req, res) {
+    if (!req.body.email) {
+      return res.status(400).send({
+        error: 'Email is required',
+      });
+    } else if (!req.body.password) {
+      return res.status(400).send({
+        error: 'Password is required',
+      });
+    }
+    const Uemail = req.body.email;
+    const rand = String(Math.random());
+    const token = `hk${ rand.slice(2, 16)}_bj`;
+
+    const logins = users.filter(user => Uemail === user.email);
+    console.log(logins);
+    if (logins) {
+      const user = {
+        id: parseInt(req.body.id),
+        token,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+      };
+      return res.status(200).send({
+        success: 'true',
+        message: 'Logged In successfully',
+        user,
+      });
+    }
+    res.status(400).send({
+      error: 'User do not exist, make sure you enter credentials correctly',
+    });
+  }
+  
+}
+
+const userController = new UsersController();
+module.exports = userController;
---
+++ Server/routes/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const userController = require('../controllers/user');
+
+const router = express.Router();
+
+router.post('/api/v1/auth/signin', userController.LoginUser);
+
+module.exports = router;
