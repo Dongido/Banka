81d11239243ae7b2ada97135c9cba0f23ffa2d9f
Gideon <chima.sima@gmail.com>
2019-04-15 23:06:19 +0100

    Feature: [#165307887] Add API endpoint for admin/staff to delete bank account

---
+++ Server/routes/account.js
@@ -0,0 +1,12 @@
+/* eslint-disable semi */
+const express = require('express');
+const accountController = require('../controllers/account');
+
+const route = express.Router();
+
+route.get('/api/v1/accounts', accountController.getAllAccounts);
+route.get('/api/v1/accounts/:id', accountController.getAccount);
+route.post('/api/v1/accounts', accountController.createAccount);
+route.delete('/api/v1/account/:AcctNo', accountController.deleteAccount);
+
+module.exports = route;
