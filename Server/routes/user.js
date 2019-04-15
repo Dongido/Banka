29c4ea325c63b69674c5debd74677c8033073740
Gideon <chima.sima@gmail.com>
2019-04-15 12:26:16 +0100

    [finishes #165075086] Create API for user sign in

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -3,6 +3,7 @@ const userController = require('../controllers/user');
 
 const router = express.Router();
 
- router.post('/api/v1/auth/signin', userController.LoginUser);
+router.post('/api/v1/auth/signup', userController.createUser);
+router.post('/api/v1/auth/signin', userController.LoginUser);
 
 module.exports = router;
