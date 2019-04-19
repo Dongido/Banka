af35d4e374476566f48160f3869d4dc139184fa0
Gideon <chima.sima@gmail.com>
2019-04-19 10:58:05 +0100

    feature(update user profile): - a user can update his profile

--- Server/controllers/user.js
+++ Server/controllers/user.js
@@ -82,7 +82,7 @@ class UsersController {
     });
   }
 
-    // UPDATE USER INFO --FUNCTION
+    //UPDATE USER INFO -- FUNCTION
   updateUser(req, res) {
     const id = parseInt(req.params.id, 10);
     let thisUser;
