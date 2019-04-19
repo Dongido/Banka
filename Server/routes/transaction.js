2d1cb85340deed24bb59082e257b8274add27b69
Gideon <chima.sima@gmail.com>
2019-04-19 13:00:41 +0100

    feature(credit user account) cashier should be able to credit user account [Finishes #165308048]

--- Server/routes/transaction.js
+++ Server/routes/transaction.js
@@ -3,6 +3,6 @@ const transactController = require('../controllers/transaction');
 
 const routes = express.Router();
 
-
+routes.post('/api/v1/transactions/:acctNo/credit', transactController.creditTransaction);
 
 module.exports = routes;
