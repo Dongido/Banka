a2879099a335198cc85836c3447e1e50ce45c749
Gideon <chima.sima@gmail.com>
2019-04-19 10:40:03 +0100

    Feature(get single user): - admin/ staff can view single user profile

--- Server/controllers/user.js
+++ Server/controllers/user.js
@@ -82,6 +82,22 @@ class UsersController {
     });
   }
 
+  // GET A USER --FUNCTION
+  getUser(req, res) {
+    const id = parseInt(req.params.id, 10);
+    users.map((user) => {
+      if (user.id === id) {
+        return res.status(200).send({
+          message: 'user retrieved successfully',
+          user,
+        });
+      }
+    });
+    return res.status(404).send({
+      error: 'user does not exist',
+    });
+  }
+
 }
 
 const userController = new UsersController();
